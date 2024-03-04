21dc873f2098000c1fc077f569f06abe0c31b285
Wojtek Pianka <w.pianka@bonasoft.pl>
2024-03-04 16:42:03 +0100

    payment test

---
+++ tests/payment.spec.ts
@@ -0,0 +1,33 @@
+import { test, expect } from '@playwright/test';
+import { loginData } from '../test-data/login.data';
+
+test.describe('Payment test', () => {
+  test.beforeEach(async ({ page }) => {
+    const userId = loginData.userId;
+    const userPassword = loginData.password;
+
+    await page.goto('/');
+    await page.getByTestId('login-input').fill(userId);
+    await page.getByTestId('password-input').fill(userPassword);
+    await page.getByTestId('login-button').click();
+    await page.getByRole('link', { name: 'płatności' }).click();
+  });
+
+  test('simple payment', async ({ page }) => {
+    //Arrange
+    const transferReceiver = 'Jan Nowak';
+    const transferAccount = '12 3456 7890 1234 5678 9012 3456';
+    const transferAmount = '222';
+    const expectedMessage = `Przelew wykonany! ${transferAmount},00PLN dla Jan Nowak`;
+
+    //Act
+    await page.getByTestId('transfer_receiver').fill(transferReceiver);
+    await page.getByTestId('form_account_to').fill(transferAccount);
+    await page.getByTestId('form_amount').fill(transferAmount);
+    await page.getByRole('button', { name: 'wykonaj przelew' }).click();
+    await page.getByTestId('close-button').click();
+
+    //Assert
+    await expect(page.locator('#show_messages')).toHaveText(expectedMessage);
+  });
+});
